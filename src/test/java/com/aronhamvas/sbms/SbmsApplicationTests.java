b88a65781b46a91ddad14d418ffb1e0a206e3bd5
Aron Hamvas <hamvas.aron@cloudera.com>
2022-08-13 13:19:51 +0200

    Spring Boot project init

---
+++ src/test/java/com/aronhamvas/sbms/SbmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aronhamvas.sbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
